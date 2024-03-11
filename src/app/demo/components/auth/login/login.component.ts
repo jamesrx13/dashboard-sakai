878a6da660b3493aceedaa60510b0a5f3bfa9d80
Juanesxz <juaneswilli@hotmail.com>
2024-03-11 16:56:31 -0500

    formReactive

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 import { appConfigurations } from 'src/environments/environment';
 import { AuthServices } from 'src/services/auth.service';
@@ -7,16 +8,19 @@ import { StorageManagger } from 'src/utilities/storage';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styles: [`
-        :host ::ng-deep .pi-eye,
-        :host ::ng-deep .pi-eye-slash {
-            transform:scale(1.6);
-            margin-right: 1rem;
-            color: var(--primary-color) !important;
-        }
-    `]
+    styles: [
+        `
+            :host ::ng-deep .pi-eye,
+            :host ::ng-deep .pi-eye-slash {
+                transform: scale(1.6);
+                margin-right: 1rem;
+                color: var(--primary-color) !important;
+            }
+        `,
+    ],
 })
 export class LoginComponent {
+    loginForm: FormGroup;
 
     valCheck: string[] = ['remember'];
 
@@ -24,23 +28,48 @@ export class LoginComponent {
 
     services: AuthServices = new AuthServices();
 
-    constructor(public layoutService: LayoutService) {
-        this.setUserPreferences()
+    constructor(
+        public layoutService: LayoutService,
+        private form: FormBuilder
+    ) {
+        this.setUserPreferences();
+        this.loginForm = this.form.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]],
+        });
     }
 
     setUserPreferences() {
-        const userPreferences = (new StorageManagger).getItem(appConfigurations.userPreferences, true);
+        const userPreferences = new StorageManagger().getItem(
+            appConfigurations.userPreferences,
+            true
+        );
 
-        if( userPreferences ) {
+        if (userPreferences) {
             this.layoutService.config.update(() => userPreferences);
         }
     }
 
-    loginSubmit(event: Event) {
-        event.preventDefault();
-        const formData = new FormData(event.target as HTMLFormElement);
+    // loginSubmit(event: Event) {
+    //     event.preventDefault();
+    //     const formData = new FormData(event.target as HTMLFormElement);
 
-        this.services.logIn(formData)
+    //     this.services.logIn(formData);
+    // }
 
+    loginSubmit() {
+        console.log(this.loginForm.value);
+        console.log(this.loginForm);
+
+        if (this.loginForm.valid) {
+            this.services.logIn(this.loginForm.value);
+        }
+    }
+
+    hasError(controlName: string, errorName: string) {
+        return (
+            this.loginForm.controls[controlName]?.hasError(errorName) &&
+            this.loginForm.controls[controlName]?.touched
+        );
     }
 }
--- src/app/demo/components/auth/login/login.module.ts
+++ src/app/demo/components/auth/login/login.module.ts
@@ -7,6 +7,7 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
@@ -16,8 +17,9 @@ import { InputTextModule } from 'primeng/inputtext';
         CheckboxModule,
         InputTextModule,
         FormsModule,
-        PasswordModule
+        PasswordModule,
+        ReactiveFormsModule,
     ],
-    declarations: [LoginComponent]
+    declarations: [LoginComponent],
 })
-export class LoginModule { }
+export class LoginModule {}
